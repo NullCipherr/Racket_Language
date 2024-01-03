a23176b36cf7fc4e3d5015a0d6a09add0e6ac508
Andrei Costa <141879308+NullCipherr@users.noreply.github.com>
2024-01-03 19:01:56 -0300

    Add files via upload

---
+++ Lessons/Lesson_01/1 - Introduction.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+; Calculo Lambda -> Lisp -> Scheme -> Racket
+; Racket é uma linguagem funcional, ou seja, diferente das linguagens imperativas, racket não possui mudanças de estados, tendo como base na:
+; Definição e Aplicação de funções, evitando dados mutáveis
+
+; Os programas em racket são simples e fáceis de compreender e modificar.
+; Além dos programas serem mais fáceis de paralelizar, consequentemente, melhorando a utilização do hardware.
+
